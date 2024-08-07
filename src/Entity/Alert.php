a81c74558325d6cb81f7f06d5560ff6ded3fdd66
Justin Hoffman <justin@ohmedia.ca>
2024-08-07 16:58:40 -0600

    add fields to entity

--- src/Entity/Alert.php
+++ src/Entity/Alert.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\AlertBundle\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\AlertBundle\Repository\AlertRepository;
 use OHMedia\UtilityBundle\Entity\BlameableEntityTrait;
@@ -16,6 +17,18 @@ class Alert
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $starts_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $ends_at = null;
+
     public function __toString(): string
     {
         return 'Alert #'.$this->id;
@@ -24,5 +37,53 @@ class Alert
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStartsAt(): ?\DateTimeImmutable
+    {
+        return $this->starts_at;
+    }
+
+    public function setStartsAt(?\DateTimeImmutable $starts_at): static
+    {
+        $this->starts_at = $starts_at;
+
+        return $this;
+    }
+
+    public function getEndsAt(): ?\DateTimeImmutable
+    {
+        return $this->ends_at;
+    }
+
+    public function setEndsAt(?\DateTimeImmutable $ends_at): static
+    {
+        $this->ends_at = $ends_at;
+
+        return $this;
     }
 }
