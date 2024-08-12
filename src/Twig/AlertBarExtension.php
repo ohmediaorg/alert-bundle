281a44367c0f11c9c1da76e78e549355ffd164bf
Justin Hoffman <justin@ohmedia.ca>
2024-08-12 16:10:15 -0600

    mock up the twig extension

---
+++ src/Twig/AlertBarExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OHMedia\AlertBundle\Twig;
+
+use OHMedia\AlertBundle\Repository\AlertRepository;
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AlertBarExtension extends AbstractExtension
+{
+    public function __construct(private AlertRepository $alertRepository)
+    {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('alert_bar', [$this, 'alertBar', [
+                'is_safe' => ['html'],
+                'needs_environment' => true,
+            ]]),
+        ];
+    }
+
+    public function alertBar(Environment $twig): string
+    {
+        $alert = $this->alertRepository->getActiveAlert();
+
+        if (!$alert) {
+            return '';
+        }
+
+        return $twig->render('@OHMediaAlert/alert_bar.html.twig', [
+            'alert' => $alert,
+        ]);
+    }
+}
